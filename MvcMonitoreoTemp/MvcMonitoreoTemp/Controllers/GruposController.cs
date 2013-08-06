2d11f1d30defdb2647890a248b7adcee0fb306f1
jkarlosGitHub <jkarlos.aguirre@gmail.com>
2013-08-05 19:51:49 -0700

    Agregando Grupos a un Usuario

--- MvcMonitoreoTemp/MvcMonitoreoTemp/Controllers/GruposController.cs
+++ MvcMonitoreoTemp/MvcMonitoreoTemp/Controllers/GruposController.cs
@@ -1,6 +1,7 @@
 ﻿using MvcMonitoreoTemp.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,11 +18,92 @@ namespace MvcMonitoreoTemp.Controllers
         {
             int cve_usuario = Convert.ToInt32(Session["cve_usuario"]);
 
-            string sQuery = "SELECT * FROM dbo.Grupos WHERE cve_usuario = {0}";
+            string sQuery = "select g.cve_grupo, g.nombre "+
+                            "from GruposUsuarios gu "+
+                            "inner join Grupos g on gu.idGrupo = g.cve_grupo "+
+                            "where gu.idUsuario = {0}";
             var grupos = db.Grupos.SqlQuery(sQuery, cve_usuario);
 
             return View(grupos);
         }
 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Grupos/Create
+
+        [HttpPost]
+        public ActionResult Create(Grupo grupo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Grupos.Add(grupo);
+                db.SaveChanges();
+
+                grupo.insertGruposUsuarios(grupo.cve_grupo, Convert.ToInt32(Session["cve_usuario"]));
+                return RedirectToAction("Index");
+            }
+
+            return View(grupo);
+        }
+
+        //
+        // GET: /Grupos/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Grupo grupo = db.Grupos.Find(id);
+            if (grupo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grupo);
+        }
+
+        //
+        // POST: /Grupos/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Grupo grupo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(grupo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(grupo);
+        }
+
+        //
+        // GET: /Grupos/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Grupo grupo = db.Grupos.Find(id);
+            if (grupo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grupo);
+        }
+
+        //
+        // POST: /Grupos/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Grupo grupo = db.Grupos.Find(id);
+
+            grupo.deleteGruposUsuarios(grupo.cve_grupo, Convert.ToInt32(Session["cve_usuario"]));
+
+            db.Grupos.Remove(grupo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
--- MvcMonitoreoTemp/MvcMonitoreoTemp/Models/Grupo.cs
+++ MvcMonitoreoTemp/MvcMonitoreoTemp/Models/Grupo.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
+
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using Microsoft.Practices.EnterpriseLibrary.Common;
+using System.Data.Common;
+using System.Data;
 
 namespace MvcMonitoreoTemp.Models
 {
+    [Table("Grupos")]
     public class Grupo
     {
         [Key]
         public int cve_grupo { get; set; }
         public string nombre { get; set; }
-        public int cve_usuario { get; set; }
+        //public int cve_usuario { get; set; }
+
+        public void insertGruposUsuarios(int idGrupo, int idUsuario)
+        {
+            Microsoft.Practices.EnterpriseLibrary.Data.Database db = DatabaseFactory.CreateDatabase("MonitoreoEntities");
+            string sqlCommand = "dbo.sp_insertGruposUsuarios";
+            DbCommand dbCommand = db.GetStoredProcCommand(sqlCommand);
+            db.AddInParameter(dbCommand, "idGrupo", DbType.String, idGrupo);
+            db.AddInParameter(dbCommand, "idUsuario", DbType.String, idUsuario);
+            db.ExecuteNonQuery(dbCommand);
+        }
+
+        public void deleteGruposUsuarios(int idGrupo, int idUsuario)
+        {
+            Microsoft.Practices.EnterpriseLibrary.Data.Database db = DatabaseFactory.CreateDatabase("MonitoreoEntities");
+            string sqlCommand = "dbo.sp_deleteGruposUsuarios";
+            DbCommand dbCommand = db.GetStoredProcCommand(sqlCommand);
+            db.AddInParameter(dbCommand, "idGrupo", DbType.String, idGrupo);
+            db.AddInParameter(dbCommand, "idUsuario", DbType.String, idUsuario);
+            db.ExecuteNonQuery(dbCommand);
+        }
     }
 }
